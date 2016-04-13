ce4fdc75dd8a0c2bf48d11285c6fceb1b3428311
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-04-13 11:18:24 +0200

    Create StabilityOracle and add it to Peer

--- src/src/epto/StabilityOracle.java
+++ src/src/epto/StabilityOracle.java
@@ -1,7 +1,25 @@
 package epto;
 
+import epto.utilities.Event;
+
 /**
  * Created by jocelyn on 12.04.16.
  */
 public class StabilityOracle {
+
+    int logicalClock = 0;
+
+    public boolean isDeliverable(Event event) {
+        return event.getTtl() > DisseminationComponent.getTTL();
+    }
+
+    public int incrementAndGetClock() {
+        logicalClock++;
+        return logicalClock;
+    }
+
+    public void updateClock(int ts) {
+        if (ts > logicalClock)
+            logicalClock = ts;
+    }
 }
