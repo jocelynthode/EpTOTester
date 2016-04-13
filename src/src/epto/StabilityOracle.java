a5926c352748a18053859a909cc77bb7c0b6d1c8
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-04-13 09:31:01 +0200

    Start Epto structure

---
+++ src/src/epto/StabilityOracle.java
@@ -0,0 +1,7 @@
+package epto;
+
+/**
+ * Created by jocelyn on 12.04.16.
+ */
+public class StabilityOracle {
+}
---
+++ src/src/epto/utilities/Event.java
@@ -0,0 +1,7 @@
+package epto.utilities;
+
+/**
+ * Created by jocelyn on 12.04.16.
+ */
+public class Event {
+}
