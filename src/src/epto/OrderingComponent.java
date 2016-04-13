ec3bd78f40069a7f80290e839eab71b1bb9b5c32
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-04-13 10:26:53 +0200

    Start OrderingComponent

--- src/src/epto/OrderingComponent.java
+++ src/src/epto/OrderingComponent.java
@@ -1,7 +1,16 @@
 package epto;
 
+import epto.utilities.Event;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by jocelyn on 12.04.16.
  */
 public class OrderingComponent {
+
+    private Map<Integer, Event> received = new HashMap<>();
+    private Map<Integer, Event> delivered = new HashMap<>();
+    private long lastDeliveredTs = 0;
 }
