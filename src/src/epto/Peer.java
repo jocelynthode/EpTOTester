549337f821d81e5b76ab59d265d62761c4b1e01d
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-04-13 19:50:45 +0200

    Add arguments for Dissemination initialization

--- src/src/epto/Peer.java
+++ src/src/epto/Peer.java
@@ -6,21 +6,26 @@ import net.sf.neem.impl.Transport;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.channels.SocketChannel;
+import java.util.Random;
 
 /**
  * Created by jocelyn on 13.04.16.
  */
 public class Peer extends Connection{
 
+    private final int DELTA = 5;
     StabilityOracle oracle = new StabilityOracle();
     OrderingComponent orderingComponent = new OrderingComponent();
-    DisseminationComponent disseminationComponent = new DisseminationComponent();
+    DisseminationComponent disseminationComponent;
 
     Peer(Transport trans, InetSocketAddress bind, InetSocketAddress remote) throws IOException {
         super(trans, bind, remote);
+        new DisseminationComponent(new Random(), trans, DELTA, oracle);
+
     }
 
     Peer(Transport trans, SocketChannel sock) throws IOException {
         super(trans, sock);
+        new DisseminationComponent(new Random(), trans, DELTA, oracle);
     }
 }
