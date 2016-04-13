947ae8df68a7e3757f01f81abe9174ea9516d540
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-04-13 10:27:32 +0200

    Implement Peer constructors

--- src/src/epto/Peer.java
+++ src/src/epto/Peer.java
@@ -1,10 +1,25 @@
 package epto;
 
 import net.sf.neem.impl.Connection;
+import net.sf.neem.impl.Transport;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SocketChannel;
 
 /**
  * Created by jocelyn on 13.04.16.
  */
-public class Peer {
+public class Peer extends Connection{
 
+    OrderingComponent orderingComponent = new OrderingComponent();
+    DisseminationComponent disseminationComponent = new DisseminationComponent();
+
+    Peer(Transport trans, InetSocketAddress bind, InetSocketAddress remote) throws IOException {
+        super(trans, bind, remote);
+    }
+
+    Peer(Transport trans, SocketChannel sock) throws IOException {
+        super(trans, sock);
+    }
 }
