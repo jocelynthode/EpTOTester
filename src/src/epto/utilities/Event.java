63c42c97f41fbc18ba777e2ef31f1600abca582a
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-04-13 11:18:51 +0200

    Add Event structure

--- src/src/epto/utilities/Event.java
+++ src/src/epto/utilities/Event.java
@@ -4,4 +4,47 @@ package epto.utilities;
  * Created by jocelyn on 12.04.16.
  */
 public class Event {
+    private int id;
+    private long timeStamp;
+    private int ttl;
+    private int sourceId;
+
+    public Event(int id, long timeStamp, int ttl, int sourceId) {
+        this.id = id;
+        this.timeStamp = timeStamp;
+        this.ttl = ttl;
+        this.sourceId = sourceId;
+    }
+
+    public int getSourceId() {
+        return sourceId;
+    }
+
+    public void setSourceId(int sourceId) {
+        this.sourceId = sourceId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public int getTtl() {
+        return ttl;
+    }
+
+    public void setTtl(int ttl) {
+        this.ttl = ttl;
+    }
+
+    public void incrementTtl() {
+        this.ttl++;
+    }
 }
