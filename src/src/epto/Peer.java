f56140e62cca1e7a1f1b63c72d6c342488586595
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-04-22 19:46:07 +0200

    Modify slightly Peer

--- src/src/epto/Peer.java
+++ src/src/epto/Peer.java
@@ -13,7 +13,7 @@ import java.util.Random;
 /**
  * Implementation of a peer as described in EpTO. This class implements the structure of a peer.
  */
-public class Peer extends Connection{
+public class Peer extends Thread{
 
     public final static int DELTA = 5;
     private StabilityOracle oracle = new StabilityOracle();
@@ -21,30 +21,17 @@ public class Peer extends Connection{
     private DisseminationComponent disseminationComponent;
     private final MulticastChannel neem;
 
-
     /**
      * Initializes a peer
      * @param trans
-     * @param bind
-     * @param remote
-     * @throws IOException
      */
-    Peer(Transport trans, InetSocketAddress bind, InetSocketAddress remote, MulticastChannel neem) throws IOException {
-        super(trans, bind, remote);
+    Peer(Transport trans, MulticastChannel neem){
         this.neem = neem;
-        new DisseminationComponent(new Random(), trans, oracle, this, neem, orderingComponent);
+        this.disseminationComponent = new DisseminationComponent(new Random(), trans, oracle, this, neem, orderingComponent);
     }
 
-    /**
-     * Initializes a peer
-     *
-     * @param trans
-     * @param sock
-     * @throws IOException
-     */
-    Peer(Transport trans, SocketChannel sock, MulticastChannel neem) throws IOException {
-        super(trans, sock);
-        this.neem = neem;
-        new DisseminationComponent(new Random(), trans, oracle, this, neem, orderingComponent);
+    @Override
+    public void run() {
+        //TODO maybe dissemination and ordering should be thread instead of this
     }
 }
