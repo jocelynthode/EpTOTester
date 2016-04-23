b98a287f5cac39da4ee45c27afc3a56a18e7d180
sam <sam_tdr@yahoo.com>
2016-04-23 18:41:50 +0200

    add App class

--- src/src/epto/utilities/App.java
+++ src/src/epto/utilities/App.java
@@ -3,9 +3,16 @@ package epto.utilities;
 
 import net.sf.neem.impl.Application;
 
-import java.nio.ByteBuffer;
+    import java.nio.ByteBuffer;
 
+/**
+ * Implementation of an Application
+ */
 public class App implements Application{
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void deliver(ByteBuffer[] byteBuffers) {
 
