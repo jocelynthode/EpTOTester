20525e8a361e44c9e787244dda1ad3cce7ca782d
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-04-26 10:12:55 +0200

    Start workin on the App logic

--- src/src/epto/Peer.java
+++ src/src/epto/Peer.java
@@ -1,8 +1,8 @@
 package epto;
 
+import epto.utilities.App;
 import epto.utilities.Event;
 import net.sf.neem.MulticastChannel;
-import net.sf.neem.apps.Addresses;
 
 import java.io.ByteArrayInputStream;
 import java.io.ObjectInputStream;
@@ -29,10 +29,10 @@ public class Peer implements Runnable{
      *
      * @param neem MultiCast object
      */
-    Peer(MulticastChannel neem){
+    public Peer(MulticastChannel neem, App app){
         this.neem = neem;
         this.oracle = new StabilityOracle();
-        this.orderingComponent = new OrderingComponent(oracle);
+        this.orderingComponent = new OrderingComponent(oracle, app);
         this.uuid = neem.getProtocolMBean().getLocalId();
         this.disseminationComponent = new DisseminationComponent(new Random(), neem.getNet(), oracle, this, neem, orderingComponent);
         neem.getProtocolMBean().setGossipFanout(DisseminationComponent.K);
@@ -52,41 +52,12 @@ public class Peer implements Runnable{
                 ByteBuffer bb = ByteBuffer.wrap(buf);
 
                 neem.read(bb);
-                ByteArrayInputStream byteIn = new ByteArrayInputStream(buf); //TODO Check correctness
+                ByteArrayInputStream byteIn = new ByteArrayInputStream(bb.array());
                 ObjectInputStream in = new ObjectInputStream(byteIn);
                 disseminationComponent.receive((HashMap<UUID, Event>) in.readObject());
             }
         } catch (AsynchronousCloseException ace) {
             // Exiting.
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String[] args) {
-        if (args.length < 1) {
-            System.err.println("Usage: apps.Chat local peer1 ... peerN");
-            System.exit(1);
-        }
-
-        try {
-
-            MulticastChannel neem = new MulticastChannel(Addresses.parse(args[0], true));
-
-            System.out.println("Started: " + neem.getLocalSocketAddress());
-
-            if (neem.getLocalSocketAddress().getAddress().isLoopbackAddress())
-                System.out.println("WARNING: Hostname resolves to loopback address! Please fix network configuration\nor expect only local peers to connect.");
-
-            //Todo should use start ?
-            Peer peer = new Peer(neem);
-            new Thread(peer).start();
-
-            for (int i = 1; i < args.length; i++)
-                neem.connect(Addresses.parse(args[i], false));
-
-            neem.close();
-
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/src/epto/utilities/App.java
+++ src/src/epto/utilities/App.java
@@ -1,12 +1,19 @@
 package epto.utilities;
 
 
+import epto.Peer;
+import net.sf.neem.MulticastChannel;
+import net.sf.neem.apps.Addresses;
 import net.sf.neem.impl.Application;
 
-    import java.nio.ByteBuffer;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.nio.ByteBuffer;
 
 /**
  * Implementation of an Application
+ * TODO broadcast + create a start application
  */
 public class App implements Application{
 
@@ -15,6 +22,49 @@ public class App implements Application{
      */
     @Override
     public void deliver(ByteBuffer[] byteBuffers) {
+        for (ByteBuffer byteBuffer : byteBuffers) {
+            byte[] content = byteBuffer.array();
+            ByteArrayInputStream byteIn = new ByteArrayInputStream(content);
+            try {
+                ObjectInputStream in = new ObjectInputStream(byteIn);
+                Event event = (Event) in.readObject();
+                System.out.println(event.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.err.println("Usage: apps.Chat local peer1 ... peerN");
+            System.exit(1);
+        }
+
+        try {
+
+            MulticastChannel neem = new MulticastChannel(Addresses.parse(args[0], true));
+
+            System.out.println("Started: " + neem.getLocalSocketAddress());
+
+            if (neem.getLocalSocketAddress().getAddress().isLoopbackAddress())
+                System.out.println("WARNING: Hostname resolves to loopback address! Please fix network configuration\nor expect only local peers to connect.");
+
+            App app = new App();
+            //Todo should use start ?
+            Peer peer = new Peer(neem, app);
+            new Thread(peer).start();
+
+            for (int i = 1; i < args.length; i++)
+                neem.connect(Addresses.parse(args[i], false));
+
+            neem.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
