af50941247c5ad7b4873815836fb500f376a1408
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-05-03 22:11:13 +0200

    Fix indentation

--- scripts/test-epto.rb
+++ scripts/test-epto.rb
@@ -3,9 +3,9 @@
 ports = [*8000..8001]
 
 (0..1).each { |i|
-    args = ports.map { |port| 'localhost:' + port.to_s }
-    ports.rotate!
-    args = args.join(' ')
-    puts "./gradlew run #{args} > localhost#{i}.txt 2>&1 &\n"
-    `./gradlew run #{args} > localhost#{i}.txt 2>&1 &`
+  args = ports.map { |port| 'localhost:' + port.to_s }
+  ports.rotate!
+  args = args.join(' ')
+  puts "java -cp ../lib/neem-0.8-jar-with-dependencies.jar:../build/libs/epto-1.0-SNAPSHOT.jar epto.utilities.App #{args} > localhost#{i}.txt 2>&1 &\n"
+  `java -cp ../lib/neem-0.8-jar-with-dependencies.jar:../build/libs/epto-1.0-SNAPSHOT.jar epto.utilities.App #{args} > localhost#{i}.txt 2>&1 &`
 }
