4b5b1fab3d40e3fae2850828ff3d192556d35a8c
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-05-04 16:48:10 +0200

    Improve Ordering Test coverage

--- src/test/java/OrderingTest.java
+++ src/test/java/OrderingTest.java
@@ -27,20 +27,27 @@ public class OrderingTest {
     private Event event5;
     private Event event6;
     private Event event7;
+    private Event event8;
+    private Event event9;
 
     private TestApp app;
     private OrderingComponent orderingComponent;
 
     @Before
     public void setUp() throws IOException {
-        event = new Event(new UUID(23333,233123),1,51,new UUID(1111,2222));
+        //TODO rethink ordering
+        event = new Event(new UUID(23333,233123),1,37,new UUID(1111,2222));
         event1 = new Event(new UUID(44444,324645),2,51,new UUID(4444,2222));
         event2 = new Event(new UUID(847392,848123),1,51,new UUID(4444,2222));
         event3 = new Event(new UUID(45775,233123),1,51,new UUID(2221,2222));
         event4 = new Event(new UUID(9823498,3409834),1,51,new UUID(11,32344));
         event5 = new Event(new UUID(439495775,34034),1,51,new UUID(22,34048488));
-        event6 = new Event(new UUID(423441,340734),4,30,new UUID(2223345,34048488));
-        event7 = new Event(new UUID(15546,98734732),2,30,new UUID(2223345,34048488));
+
+        event6 = new Event(new UUID(423441,340734),4,30,new UUID(2223345,3426356));
+        event7 = new Event(new UUID(15546,98734732),2,30,new UUID(2223345,34566998));
+        event8 = new Event(new UUID(8384834,34343l),5,60,new UUID(22,34048488));
+        event9 = new Event(new UUID(23333,233123),1,51,new UUID(1111,2222));
+
         app = new TestApp();
         orderingComponent = new OrderingComponent(new StabilityOracle(50), app);
     }
@@ -54,20 +61,30 @@ public class OrderingTest {
             put(event3.getId(), event3);
             put(event4.getId(), event4);
             put(event5.getId(), event5);
+        }};
+
+        ConcurrentHashMap<UUID, Event> map1 = new ConcurrentHashMap<UUID, Event>(){{
+            put(event9.getId(), event9);
             put(event6.getId(), event6);
+            put(event7.getId(), event7);
+            put(event8.getId(), event8);
         }};
 
         orderingComponent.orderEvents(map);
+        orderingComponent.orderEvents(map1);
+        orderingComponent.orderEvents(new ConcurrentHashMap<>());
 
         Assert.assertTrue(app.events.size() == 6);
         Assert.assertEquals(event4.getId(), app.events.get(0));
         Assert.assertEquals(event5.getId(), app.events.get(1));
-        Assert.assertEquals(event.getId(), app.events.get(2));
-        Assert.assertEquals(event3.getId(), app.events.get(3));
-        Assert.assertEquals(event2.getId(), app.events.get(4));
+        Assert.assertEquals(event3.getId(), app.events.get(2));
+        Assert.assertTrue(event9.getTtl() > 37 );
+        Assert.assertEquals(event2.getId(), app.events.get(3));
+        Assert.assertEquals(event9.getId(), app.events.get(4));
         Assert.assertEquals(event1.getId(), app.events.get(5));
         Assert.assertFalse(app.events.contains(event6.getId()));
         Assert.assertFalse(app.events.contains(event7.getId()));
+        Assert.assertFalse(app.events.contains(event8.getId()));
     }
 
     private class TestApp implements Application {
