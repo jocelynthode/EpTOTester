0485bd11cb77b3af22c48a1f241051fb9585f430
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-05-06 19:08:09 +0200

    Use same sets for testing

--- src/test/java/DisseminationTest.java
+++ src/test/java/DisseminationTest.java
@@ -18,46 +18,88 @@ public class DisseminationTest {
     private MockApp app;
     private MockApp app1;
     private MockApp app2;
+    private MockApp app3;
+    private MockApp app4;
+    private MockApp app5;
     private MulticastChannel neem;
     private MulticastChannel neem1;
     private MulticastChannel neem2;
+    private MulticastChannel neem3;
+    private MulticastChannel neem4;
+    private MulticastChannel neem5;
 
     @Before
     public void setUp() throws IOException {
 
-        final int ttl = 20;
-        final int k = 3;
+        final int ttl = 30;
+        final int k = 4;
 
         InetSocketAddress address10000 = new InetSocketAddress("localhost", 10000);
         InetSocketAddress address10001 = new InetSocketAddress("localhost", 10001);
         InetSocketAddress address10002 = new InetSocketAddress("localhost", 10002);
+        InetSocketAddress address10003 = new InetSocketAddress("localhost", 10003);
+        InetSocketAddress address10004 = new InetSocketAddress("localhost", 10004);
+        InetSocketAddress address10005 = new InetSocketAddress("localhost", 10005);
+
+        InetSocketAddress[] addresses0 = new InetSocketAddress[] {address10001, address10002, address10003,
+                address10004, address10005};
+        InetSocketAddress[] addresses1 = new InetSocketAddress[] {address10000, address10002, address10003,
+                address10004, address10005};
+        InetSocketAddress[] addresses2 = new InetSocketAddress[] {address10000, address10001, address10003,
+                address10004, address10005};
+        InetSocketAddress[] addresses3 = new InetSocketAddress[] {address10000, address10001, address10002,
+                address10004, address10005};
+        InetSocketAddress[] addresses4 = new InetSocketAddress[] {address10000, address10001, address10002, address10003,
+                address10005};
+        InetSocketAddress[] addresses5 = new InetSocketAddress[] {address10000, address10001, address10002, address10003,
+                address10004};
 
         neem = new MulticastChannel(address10000);
         neem1 = new MulticastChannel(address10001);
         neem2 = new MulticastChannel(address10002);
+        neem3 = new MulticastChannel(address10003);
+        neem4 = new MulticastChannel(address10004);
+        neem5 = new MulticastChannel(address10005);
 
-        neem.connect(address10001);
-        neem.connect(address10002);
-        neem1.connect(address10000);
-        neem1.connect(address10002);
-        neem2.connect(address10000);
-        neem2.connect(address10001);
+        for (InetSocketAddress address: addresses0) {
+            neem.connect(address);
+        }
+
+        for (InetSocketAddress address: addresses1) {
+            neem1.connect(address);
+        }
+
+        for (InetSocketAddress address: addresses2) {
+            neem2.connect(address);
+        }
+
+        for (InetSocketAddress address: addresses3) {
+            neem3.connect(address);
+        }
+
+        for (InetSocketAddress address: addresses4) {
+            neem4.connect(address);
+        }
+
+        for (InetSocketAddress address: addresses5) {
+            neem5.connect(address);
+        }
+
 
         app = new MockApp(neem, ttl, k);
         app1 = new MockApp(neem1, ttl, k);
         app2 = new MockApp(neem2, ttl, k);
+        app3 = new MockApp(neem3, ttl, k);
+        app4 = new MockApp(neem4, ttl, k);
+        app5 = new MockApp(neem5, ttl, k);
 
         app.start();
         app1.start();
         app2.start();
+        app3.start();
+        app4.start();
+        app5.start();
 
-    }
-
-    @After
-    public void tearDown() {
-        neem.close();
-        neem1.close();
-        neem2.close();
     }
 
 
@@ -65,19 +107,34 @@ public class DisseminationTest {
     @Test
     public void testDissemination() throws Exception {
 
-        app.broadcast(new Event(new UUID(11911, 22292), 0, 0, null));
-        app1.broadcast(new Event(new UUID(112511, 255222), 0, 0, null));
-        app1.broadcast(new Event(new UUID(11151, 225345222), 0, 0, null));
-        app.broadcast(new Event(new UUID(1152311, 22522), 0, 0, null));
-        app2.broadcast(new Event(new UUID(19111, 225422), 0, 0, null));
-        app1.broadcast(new Event(new UUID(115511, 22292), 0, 0, null));
-        app.broadcast(new Event(new UUID(11234511, 2222), 0, 0, null));
-        app2.broadcast(new Event(new UUID(11, 22252), 0, 0, null));
+        Event event = new Event(new UUID(23333, 233123), 0, 0, null);
+        Event event1 = new Event(new UUID(44444, 324645), 0, 0, null);
+        Event event2 = new Event(new UUID(847392, 848123), 0, 0, null);
+        Event event3 = new Event(new UUID(45775, 233123), 0, 0, null);
+        Event event4 = new Event(new UUID(9823498, 3409834), 0, 0, null);
+        Event event5 = new Event(new UUID(439495775, 34034), 0, 0, null);
+        Event event6 = new Event(new UUID(543234, 23794), 0, 0, null);
+        Event event7 = new Event(new UUID(63567, 874), 0, 0, null);
+        Event event8 = new Event(new UUID(23457, 34470734), 0, 0, null);
+        Event event9 = new Event(new UUID(562, 57), 0, 0, null);
+        Event event10 = new Event(new UUID(23456, 340735534), 0, 0, null);
+
+        app.broadcast(event);
+        app1.broadcast(event1);
+        app.broadcast(event2);
+        app2.broadcast(event3);
+        app1.broadcast(event4);
+        app2.broadcast(event5);
+        app3.broadcast(event6);
+        app4.broadcast(event7);
+        app5.broadcast(event8);
+        app4.broadcast(event9);
+        app3.broadcast(event10);
 
         Thread.sleep(10000);
 
-        Assert.assertTrue(app.getPeer().getOrderingComponent().getReceived().size() == 8);
-        Assert.assertTrue(app1.getPeer().getOrderingComponent().getReceived().size() == 8);
-        Assert.assertTrue(app2.getPeer().getOrderingComponent().getReceived().size() == 8);
+        Assert.assertTrue(app.getPeer().getOrderingComponent().getReceived().size() == 11);
+        Assert.assertTrue(app1.getPeer().getOrderingComponent().getReceived().size() == 11);
+        Assert.assertTrue(app2.getPeer().getOrderingComponent().getReceived().size() == 11);
     }
 }
--- src/test/java/ProtocolTest.java
+++ src/test/java/ProtocolTest.java
@@ -17,38 +17,87 @@ public class ProtocolTest {
     private MockApp app;
     private MockApp app1;
     private MockApp app2;
+    private MockApp app3;
+    private MockApp app4;
+    private MockApp app5;
     private MulticastChannel neem;
     private MulticastChannel neem1;
     private MulticastChannel neem2;
+    private MulticastChannel neem3;
+    private MulticastChannel neem4;
+    private MulticastChannel neem5;
 
     @Before
     public void setUp() throws Exception {
 
         final int ttl = 30;
-        final int k = 10;
+        final int k = 4;
 
         InetSocketAddress address10000 = new InetSocketAddress("localhost", 10000);
         InetSocketAddress address10001 = new InetSocketAddress("localhost", 10001);
         InetSocketAddress address10002 = new InetSocketAddress("localhost", 10002);
+        InetSocketAddress address10003 = new InetSocketAddress("localhost", 10003);
+        InetSocketAddress address10004 = new InetSocketAddress("localhost", 10004);
+        InetSocketAddress address10005 = new InetSocketAddress("localhost", 10005);
+
+        InetSocketAddress[] addresses0 = new InetSocketAddress[] {address10001, address10002, address10003,
+                address10004, address10005};
+        InetSocketAddress[] addresses1 = new InetSocketAddress[] {address10000, address10002, address10003,
+                address10004, address10005};
+        InetSocketAddress[] addresses2 = new InetSocketAddress[] {address10000, address10001, address10003,
+                address10004, address10005};
+        InetSocketAddress[] addresses3 = new InetSocketAddress[] {address10000, address10001, address10002,
+                address10004, address10005};
+        InetSocketAddress[] addresses4 = new InetSocketAddress[] {address10000, address10001, address10002, address10003,
+                address10005};
+        InetSocketAddress[] addresses5 = new InetSocketAddress[] {address10000, address10001, address10002, address10003,
+                address10004};
 
         neem = new MulticastChannel(address10000);
         neem1 = new MulticastChannel(address10001);
         neem2 = new MulticastChannel(address10002);
+        neem3 = new MulticastChannel(address10003);
+        neem4 = new MulticastChannel(address10004);
+        neem5 = new MulticastChannel(address10005);
 
-        neem.connect(address10001);
-        neem.connect(address10002);
-        neem1.connect(address10000);
-        neem1.connect(address10002);
-        neem2.connect(address10000);
-        neem2.connect(address10001);
+        for (InetSocketAddress address: addresses0) {
+            neem.connect(address);
+        }
+
+        for (InetSocketAddress address: addresses1) {
+            neem1.connect(address);
+        }
+
+        for (InetSocketAddress address: addresses2) {
+            neem2.connect(address);
+        }
+
+        for (InetSocketAddress address: addresses3) {
+            neem3.connect(address);
+        }
+
+        for (InetSocketAddress address: addresses4) {
+            neem4.connect(address);
+        }
+
+        for (InetSocketAddress address: addresses5) {
+            neem5.connect(address);
+        }
+
 
         app = new MockApp(neem, ttl, k);
         app1 = new MockApp(neem1, ttl, k);
         app2 = new MockApp(neem2, ttl, k);
+        app3 = new MockApp(neem3, ttl, k);
+        app4 = new MockApp(neem4, ttl, k);
+        app5 = new MockApp(neem5, ttl, k);
 
         app.start();
         app1.start();
         app2.start();
+        app3.start();
+        app4.start();
+        app5.start();
 
     }
 
@@ -62,13 +111,18 @@ public class ProtocolTest {
     //TODO for now compare if order is the same everywhere maybe make it so we can compare order too
     @Test
     public void testProtocol() throws Exception {
+
         Event event = new Event(new UUID(23333, 233123), 0, 0, null);
         Event event1 = new Event(new UUID(44444, 324645), 0, 0, null);
         Event event2 = new Event(new UUID(847392, 848123), 0, 0, null);
         Event event3 = new Event(new UUID(45775, 233123), 0, 0, null);
-        Event event4 = new Event(new UUID(9823498, 3409834), 0, 49, null);
-        Event event5 = new Event(new UUID(439495775, 34034), 0, 49, null);
-        Event event6 = new Event(new UUID(423441, 340734), 0, 0, null);
+        Event event4 = new Event(new UUID(9823498, 3409834), 0, 0, null);
+        Event event5 = new Event(new UUID(439495775, 34034), 0, 0, null);
+        Event event6 = new Event(new UUID(543234, 23794), 0, 0, null);
+        Event event7 = new Event(new UUID(63567, 874), 0, 0, null);
+        Event event8 = new Event(new UUID(23457, 34470734), 0, 0, null);
+        Event event9 = new Event(new UUID(562, 57), 0, 0, null);
+        Event event10 = new Event(new UUID(23456, 340735534), 0, 0, null);
 
         app.broadcast(event);
         app1.broadcast(event1);
@@ -76,18 +130,22 @@ public class ProtocolTest {
         app2.broadcast(event3);
         app1.broadcast(event4);
         app2.broadcast(event5);
-        app1.broadcast(event6);
+        app3.broadcast(event6);
+        app4.broadcast(event7);
+        app5.broadcast(event8);
+        app4.broadcast(event9);
+        app3.broadcast(event10);
 
         int retry = 0;
-        while ((retry != 10) && ((app.events.size() != 9)
-                || (app1.events.size() != 9)
-                || (app2.events.size() != 9))) {
-            Thread.sleep(10000);
+        while ((retry != 10) && ((app.events.size() != 11)
+                || (app1.events.size() != 11)
+                || (app2.events.size() != 11))) {
+            Thread.sleep(6000);
             retry++;
         }
-        Assert.assertTrue(((app.events.size() == 9)
-                && (app1.events.size() == 9)
-                && (app2.events.size() == 9)));
+        Assert.assertTrue("Not all events delivered", ((app.events.size() == 11)
+                && (app1.events.size() == 11)
+                && (app2.events.size() == 11)));
         Assert.assertArrayEquals(app.events.toArray(), app1.events.toArray());
         Assert.assertArrayEquals(app1.events.toArray(), app2.events.toArray());
 
