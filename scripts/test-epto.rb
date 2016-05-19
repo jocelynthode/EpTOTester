8be9ba447d944a9ac14ea37667861d81fe62789b
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-05-19 18:07:49 +0200

    Add ShadowJar and use it

--- scripts/test-epto.rb
+++ scripts/test-epto.rb
@@ -2,10 +2,13 @@
 
 ports = [*10000..10019]
 
+puts 'compiling...'
+puts `cd ..; ./gradlew --daemon clean shadowJar && cd scripts/;`
+
 (0..19).each { |i|
   args = ports.map { |port| 'localhost:' + port.to_s }
   ports.rotate!
   args = args.join(' ')
-  puts "java -cp ../lib/neem-0.8-jar-with-dependencies.jar:../build/libs/epto-1.0-SNAPSHOT.jar epto.utilities.App #{args} > localhost#{i}.txt 2>&1 &\n"
-  `java -cp ../lib/neem-0.8-jar-with-dependencies.jar:../build/libs/epto-1.0-SNAPSHOT.jar epto.utilities.App #{args} > localhost#{i}.txt 2>&1 &`
+  puts "java -cp ../build/libs/epto-1.0-SNAPSHOT-all.jar epto.utilities.App #{args} > localhost#{i}.txt 2>&1 &\n"
+  `java -cp ../build/libs/epto-1.0-SNAPSHOT-all.jar epto.utilities.App #{args} > localhost#{i}.txt 2>&1 &`
 }
