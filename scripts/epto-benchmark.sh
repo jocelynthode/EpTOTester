1b906c3a24c7cae8aa0f4f35c3236f8c4329c2f5
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-08-01 22:44:59 +0200

    Create own network instead of using ingress

--- scripts/epto-benchmark.sh
+++ scripts/epto-benchmark.sh
@@ -19,8 +19,11 @@ docker swarm init
 TOKEN=$(docker swarm join-token -q worker)
 parallel-ssh -h hosts "docker swarm join --token ${TOKEN} ${MANAGER_IP}:2377"
 
-docker service create --name epto-tracker --network ingress --replicas 1 --limit-memory 180m tracker
-docker service create --name epto-service --network ingress --replicas ${PEER_NUMBER} --limit-memory 180m --mount type=bind,source=/data,target=/data epto
+# If networking doesn't work use ingress
+docker network create -d overlay --subnet=10.0.9.0/24 epto-network
+
+docker service create --name epto-tracker --network epto-network --replicas 1 --limit-memory 180m tracker
+docker service create --name epto-service --network epto-network --replicas ${PEER_NUMBER} --limit-memory 180m --mount type=bind,source=/data,target=/data epto
 
 #wait for apps to finish
 for i in {1..40} :
@@ -33,6 +36,7 @@ done
 
 docker service rm epto-service
 docker service rm epto-tracker
+docker network rm epto-network
 # collect logs
 #for i in $(docker ps -aqf "ancestor=epto");do  docker cp ${i}:/opt/epto/localhost.txt ./${i}_log.txt; done
 parallel-ssh -h hosts "docker swarm leave"
