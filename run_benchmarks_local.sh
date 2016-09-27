188b3fe1e087b852421b42a443068faa9807b8fb
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-09-27 15:26:56 +0200

    Update local script

--- run_benchmarks_local.sh
+++ run_benchmarks_local.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # This scripts runs the benchmarks on a remote cluster
 
+MANAGER_IP=172.16.2.53
 PEER_NUMBER=$1
 
 
@@ -12,35 +13,37 @@ fi
 
 
 echo "START..."
+./gradlew docker
 
 # Clean everything at Ctrl+C
-trap 'docker network rm epto-network && docker swarm leave --force && exit' TERM INT
-
+trap 'docker service rm epto-service && docker service rm epto-tracker && docker network rm epto-network && \
+docker swarm leave --force && exit' TERM INT
 
 docker swarm init
 
 # If networking doesn't work use ingress
 docker network create -d overlay --subnet=10.0.93.0/24 epto-network
 
-# TODO change mount path
-docker service create --name epto-tracker --network epto-network --replicas 1 --limit-memory 180m tracker
+docker service create --name epto-tracker --network epto-network --replicas 1 --limit-memory 370m tracker
 docker service create --name epto-service --network epto-network --replicas ${PEER_NUMBER} --env "PEER_NUMBER=${PEER_NUMBER}" \
- --limit-memory 200m --mount type=bind,source=/home/jocelyn/epto-data,target=/data epto
+--limit-memory 370m --log-driver=journald --mount type=bind,source=/home/jocelyn/tmp/data,target=/data epto
+
+echo "Fleshing out the network..."
+sleep 10s
 
 #wait for apps to finish
-for i in {1..20} :
+for i in {1..60} :
 do
 	sleep 20s
     echo "waiting..."
 done
-#echo "waiting 2 more minutes"
-#sleep 2m
 
 docker service rm epto-service
 docker service rm epto-tracker
 docker network rm epto-network
-# collect logs
-#parallel-ssh -h hosts "docker swarm leave"
 docker swarm leave --force
 
-echo "finished"
+
+#while read ip; do
+#    rsync -av ${ip}:~/data/ ../data/
+#done <hosts
