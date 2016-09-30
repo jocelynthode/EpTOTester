8890f4f30d6e18b54f03e41886887fe2f2a3404a
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-09-30 11:26:11 +0200

    Remove addtime since it is given by logger now

--- scripts/container-start-script.sh
+++ scripts/container-start-script.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 # This script needs to run in the container
 
-addtime() {
-    while IFS= read -r line; do
-        echo "$(date +%s%N | cut -b1-13) $line"
-    done
-}
-
 MY_IP_ADDR=$(/bin/hostname -i)
 
 # wait for all peers
@@ -17,4 +11,4 @@ echo "${MY_IP_ADDR}"
 echo "${PEER_NUMBER}"
 MY_IP_ADDR=($MY_IP_ADDR)
 echo "${MY_IP_ADDR[0]}"
-java -Xms150m -Xmx330m -cp ./epto-1.0-SNAPSHOT-all.jar epto.utilities.Main "${MY_IP_ADDR[0]}" "http://epto-tracker:4321" "${PEER_NUMBER}" | addtime > "/data/${MY_IP_ADDR[0]}.txt" 2>&1
+java -Xms150m -Xmx330m -cp ./epto-1.0-SNAPSHOT-all.jar epto.utilities.Main "${MY_IP_ADDR[0]}" "http://epto-tracker:4321" "${PEER_NUMBER}" > "/data/${MY_IP_ADDR[0]}.txt" 2>&1
