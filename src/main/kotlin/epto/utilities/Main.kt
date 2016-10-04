2d3a424a437a4b383d2ad545e70fcde108b0957d
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-10-04 17:35:39 +0200

    Add argparse4j

--- src/main/kotlin/epto/utilities/Main.kt
+++ src/main/kotlin/epto/utilities/Main.kt
@@ -1,6 +1,9 @@
 package epto.utilities
 
 import epto.libs.Delegates.logger
+import net.sourceforge.argparse4j.ArgumentParsers
+import net.sourceforge.argparse4j.impl.Arguments
+import net.sourceforge.argparse4j.inf.ArgumentParser
 import java.net.InetAddress
 
 /**
@@ -20,10 +23,15 @@ class Main {
                 logger.error("Usage: apps.Main local tracker peer_number")
                 System.exit(1)
             }
-
-            logger.info("Started: ${args[0]}")
-            logger.info(args[1])
-            logger.info(args[2])
+/*
+            val parser = ArgumentParsers.newArgumentParser("EpTO tester")
+            parser.addArgument("-i", "--local-ip").help("Peer local IP")
+            parser.addArgument("-t", "--tracker").help("Tracker used to fetch initial view").
+                    setDefault("http://epto-tracker:4321")
+            parser.addArgument("-n", "--peer-number").help("Peer number")
+                    .type(Integer.TYPE)
+                    .setDefault(35)
+                    */
 
             //c = 4 for 99.9875% =>  c+1 = 5
             val n = args[2].toDouble()
@@ -38,12 +46,13 @@ class Main {
 
             val application = Application(ttl, k, args[1], expectedEvents, InetAddress.getByName(args[0]))
 
+            application.start()
+            //Let all docker instances be created
+            Thread.sleep(70000)
+            logger.info("Started: ${args[0]}")
             logger.info("Peer ID : ${application.peer.uuid}")
             logger.info("Peer Number : ${n.toInt()}")
             logger.info("TTL : $ttl, K : $k")
-
-            application.start()
-            Thread.sleep(10000)
 
             var eventsSent = 0
             while (eventsSent != EVENTS_TO_SEND) {
