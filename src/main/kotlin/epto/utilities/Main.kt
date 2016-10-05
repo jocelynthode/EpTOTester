90c47cbde987b55b653fa9283561738a1fa32946
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-10-05 10:10:14 +0200

    Use argparse4j and a shutdownHook to write messages transmitted

--- src/main/kotlin/epto/utilities/Main.kt
+++ src/main/kotlin/epto/utilities/Main.kt
@@ -2,9 +2,10 @@ package epto.utilities
 
 import epto.libs.Delegates.logger
 import net.sourceforge.argparse4j.ArgumentParsers
-import net.sourceforge.argparse4j.impl.Arguments
-import net.sourceforge.argparse4j.inf.ArgumentParser
+import net.sourceforge.argparse4j.inf.ArgumentParserException
+import net.sourceforge.argparse4j.inf.Namespace
 import java.net.InetAddress
+import kotlin.concurrent.thread
 
 /**
  * Created by jocelyn on 19.09.16.
@@ -14,48 +15,80 @@ class Main {
     companion object {
 
         val logger by logger()
-
-        const val EVENTS_TO_SEND = 12
         var expectedEvents = 0
 
         @JvmStatic fun main(args: Array<String>) {
-            if (args.size < 3) {
-                logger.error("Usage: apps.Main local tracker peer_number")
-                System.exit(1)
-            }
-/*
+
             val parser = ArgumentParsers.newArgumentParser("EpTO tester")
-            parser.addArgument("-i", "--local-ip").help("Peer local IP")
-            parser.addArgument("-t", "--tracker").help("Tracker used to fetch initial view").
+            parser.defaultHelp(true)
+            parser.addArgument("localIp").help("Peer local IP")
+            parser.addArgument("tracker").help("Tracker used to fetch initial view").
                     setDefault("http://epto-tracker:4321")
-            parser.addArgument("-n", "--peer-number").help("Peer number")
+            parser.addArgument("peerNumber").help("Peer number")
                     .type(Integer.TYPE)
                     .setDefault(35)
-                    */
+            parser.addArgument("-e", "--events").help("Number of events to send")
+                    .type(Integer.TYPE)
+                    .setDefault(12)
+            parser.addArgument("-k", "--fanout").help("Number of peers to gossip")
+                    .type(Integer.TYPE)
+            parser.addArgument("-t", "--ttl").help("Number of rounds before considering an event mature")
+                    .type(Integer.TYPE)
+
+
+            try {
+                val res = parser.parseArgs(args)
+                startProgram(res)
+            } catch (e: ArgumentParserException) {
+                parser.handleError(e)
+            }
+        }
+
+        @JvmStatic private fun  startProgram(namespace: Namespace) {
+
+            val eventsToSend = namespace.getInt("events")
+            val localIp = namespace.getString("localIp")
+            val tracker = namespace.getString("tracker")
+            val n = namespace.getInt("peerNumber").toDouble()
 
             //c = 4 for 99.9875% =>  c+1 = 5
-            val n = args[2].toDouble()
             val log2N = Math.log(n) / Math.log(2.0)
-            val ttl = (2 * Math.ceil(5 * log2N) + 1).toInt()
-            val k = Math.ceil(2.0 * Math.E * Math.log(n) / Math.log(Math.log(n))).toInt()
+            val ttl = if (namespace.getInt("ttl") == null) {
+                (2 * Math.ceil(5 * log2N) + 1).toInt()
+            } else {
+                namespace.getInt("ttl")
+            }
+            val k = if (namespace.getInt("fanout") == null) {
+                Math.ceil(2.0 * Math.E * Math.log(n) / Math.log(Math.log(n))).toInt()
+            } else {
+                namespace.getInt("fanout")
+            }
 
-            if (InetAddress.getByName(args[0]).isLoopbackAddress)
+            if (InetAddress.getByName(localIp).isLoopbackAddress)
                 logger.warn("WARNING: Hostname resolves to loopback address! Please fix network configuration\nor expect only local peers to connect.")
 
-            expectedEvents = EVENTS_TO_SEND * n.toInt()
+            expectedEvents = eventsToSend * n.toInt()
 
-            val application = Application(ttl, k, args[1], expectedEvents, InetAddress.getByName(args[0]))
+            val application = Application(ttl, k, tracker, expectedEvents, InetAddress.getByName(localIp))
+
+            Runtime.getRuntime().addShutdownHook(thread {
+                logger.info("Quitting EpTO tester")
+                application.stop()
+                logger.info("EpTO messages sent: ${application.peer.core.gossipMessages}")
+                logger.info("EpTO messages received: ${application.peer.messagesReceived}")
+                logger.info("PSS messages sent: ${application.peer.core.pssMessages}")
+            })
 
             application.start()
             //Let all docker instances be created
-            Thread.sleep(70000)
-            logger.info("Started: ${args[0]}")
+            logger.info("Started: $localIp")
             logger.info("Peer ID : ${application.peer.uuid}")
             logger.info("Peer Number : ${n.toInt()}")
             logger.info("TTL : $ttl, K : $k")
+            Thread.sleep(70000)
 
             var eventsSent = 0
-            while (eventsSent != EVENTS_TO_SEND) {
+            while (eventsSent != eventsToSend) {
                 application.broadcast()
                 Thread.sleep(1000)
                 eventsSent++
@@ -64,8 +97,6 @@ class Main {
                 logger.info("Events not yet delivered: ${application.peer.orderingComponent.received.size}")
                 Thread.sleep(10000)
             }
-            //neem.close();
-
         }
     }
 }
