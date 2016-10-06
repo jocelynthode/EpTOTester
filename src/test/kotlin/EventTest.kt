98af3f18f423de150eaa8ec874177af946ba2f50
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-10-06 11:33:27 +0200

    Add small tests to determine Byte size of serialized events and pss views

--- src/test/kotlin/EventTest.kt
+++ src/test/kotlin/EventTest.kt
@@ -6,7 +6,6 @@ import org.junit.Test
 import java.io.ByteArrayOutputStream
 import java.io.IOException
 import java.util.*
-import java.util.zip.GZIPOutputStream
 
 /**
  * Class testing the events methods
@@ -30,10 +29,10 @@ class EventTest {
         event2 = Event(UUID(847392, 848123), 1, 5, UUID(4444, 2222))
         event3 = Event(UUID(23333, 233123), 1, 5, UUID(1111, 2222))
 
-        event4 = Event(UUID(2951, 29942), 12523452345, 87, UUID(67754, 252453))
-        event5 = Event(UUID(444344, 324645), 94845423418, 143, UUID(3456745, 453568))
-        event6 = Event(UUID(847392, 848123), 2345, 9, UUID(2662, 57245))
-        event7 = Event(UUID(255333, 233123), 59595, 45, UUID(61856, 8454))
+        event4 = Event(UUID(2951, 29942), 500, 87, UUID(67754, 252453))
+        event5 = Event(UUID(444344, 324645), 300, 300, UUID(3456745, 453568))
+        event6 = Event(UUID(847392, 848123), 130, 9, UUID(2662, 57245))
+        event7 = Event(UUID(255333, 233123), 30, 45, UUID(61856, 8454))
     }
 
     @Test
@@ -53,29 +52,38 @@ class EventTest {
 
     @Test
     fun testEventOverhead() {
-        val byteOut = ByteArrayOutputStream()
-        val gzipOut = GZIPOutputStream(byteOut, 65536)
-        val out = Application.conf.getObjectOutput(byteOut)
-
+        var byteOut = ByteArrayOutputStream()
+        var out = Application.conf.getObjectOutput(byteOut)
 
         out.writeObject(event4, Event::class.java)
-        out.flush()
+        out.close()
         println(byteOut.toByteArray().size)
+
+        byteOut = ByteArrayOutputStream()
+        out = Application.conf.getObjectOutput(byteOut)
+
         out.writeObject(event5, Event::class.java)
-        out.flush()
+        out.close()
         println(byteOut.toByteArray().size)
+
+        byteOut = ByteArrayOutputStream()
+        out = Application.conf.getObjectOutput(byteOut)
+
         out.writeObject(event6, Event::class.java)
-        out.flush()
+        out.close()
         println(byteOut.toByteArray().size)
+
+        byteOut = ByteArrayOutputStream()
+        out = Application.conf.getObjectOutput(byteOut)
+
         out.writeObject(event7, Event::class.java)
-        out.flush()
+        out.close()
         println(byteOut.toByteArray().size)
 
         println("---------------")
 
-        val byteOut1 = ByteArrayOutputStream()
-        val gzipOut1 = GZIPOutputStream(byteOut1, 65536)
-        val out1 = Application.conf.getObjectOutput(byteOut1)
+        var byteOut1 = ByteArrayOutputStream()
+        var out1 = Application.conf.getObjectOutput(byteOut1)
 
         out1.writeLong(event4.id.mostSignificantBits)
         out1.writeLong(event4.id.leastSignificantBits)
@@ -83,62 +91,86 @@ class EventTest {
         out1.writeInt(event4.ttl)
         out1.writeLong(event4.sourceId!!.mostSignificantBits)
         out1.writeLong(event4.sourceId!!.leastSignificantBits)
-        out1.flush()
+        out1.close()
         println(byteOut1.toByteArray().size)
+
+        byteOut1 = ByteArrayOutputStream()
+        out1 = Application.conf.getObjectOutput(byteOut1)
+
         out1.writeLong(event5.id.mostSignificantBits)
         out1.writeLong(event5.id.leastSignificantBits)
         out1.writeLong(event5.timestamp)
         out1.writeInt(event5.ttl)
         out1.writeLong(event5.sourceId!!.mostSignificantBits)
         out1.writeLong(event5.sourceId!!.leastSignificantBits)
-        out1.flush()
+        out1.close()
         println(byteOut1.toByteArray().size)
+
+        byteOut1 = ByteArrayOutputStream()
+        out1 = Application.conf.getObjectOutput(byteOut1)
+
         out1.writeLong(event6.id.mostSignificantBits)
         out1.writeLong(event6.id.leastSignificantBits)
         out1.writeLong(event6.timestamp)
         out1.writeInt(event6.ttl)
         out1.writeLong(event6.sourceId!!.mostSignificantBits)
         out1.writeLong(event6.sourceId!!.leastSignificantBits)
-        out1.flush()
+        out1.close()
         println(byteOut1.toByteArray().size)
+
+        byteOut1 = ByteArrayOutputStream()
+        out1 = Application.conf.getObjectOutput(byteOut1)
+
         out1.writeLong(event7.id.mostSignificantBits)
         out1.writeLong(event7.id.leastSignificantBits)
         out1.writeLong(event7.timestamp)
         out1.writeInt(event7.ttl)
         out1.writeLong(event7.sourceId!!.mostSignificantBits)
         out1.writeLong(event7.sourceId!!.leastSignificantBits)
-        out1.flush()
+        out1.close()
         println(byteOut1.toByteArray().size)
+
 
         println("---------------")
 
-        val byteOut2 = ByteArrayOutputStream()
-        val gzipOut2 = GZIPOutputStream(byteOut2, 65536)
-        val out2 = Application.conf.getObjectOutput(byteOut2)
+        var byteOut2 = ByteArrayOutputStream()
+        var out2 = Application.conf.getObjectOutput(byteOut2)
 
         out2.writeObject(event4.id, UUID::class.java)
         out2.writeLong(event4.timestamp)
         out2.writeInt(event4.ttl)
         out2.writeObject(event4.sourceId, UUID::class.java)
-        out2.flush()
+        out2.close()
         println(byteOut2.toByteArray().size)
+
+        byteOut2 = ByteArrayOutputStream()
+        out2 = Application.conf.getObjectOutput(byteOut2)
+
         out2.writeObject(event5.id, UUID::class.java)
         out2.writeLong(event5.timestamp)
         out2.writeInt(event5.ttl)
         out2.writeObject(event5.sourceId, UUID::class.java)
-        out2.flush()
+        out2.close()
         println(byteOut2.toByteArray().size)
+
+        byteOut2 = ByteArrayOutputStream()
+        out2 = Application.conf.getObjectOutput(byteOut2)
+
         out2.writeObject(event6.id, UUID::class.java)
         out2.writeLong(event6.timestamp)
         out2.writeInt(event6.ttl)
         out2.writeObject(event6.sourceId, UUID::class.java)
-        out2.flush()
+        out2.close()
         println(byteOut2.toByteArray().size)
+
+        byteOut2 = ByteArrayOutputStream()
+        out2 = Application.conf.getObjectOutput(byteOut2)
+
         out2.writeObject(event7.id, UUID::class.java)
         out2.writeLong(event7.timestamp)
         out2.writeInt(event7.ttl)
         out2.writeObject(event7.sourceId, UUID::class.java)
-        out2.flush()
+        out2.close()
         println(byteOut2.toByteArray().size)
 
     }
