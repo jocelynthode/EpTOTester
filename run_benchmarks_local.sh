a9894be02dd7292c51250f7d611db8dddcca1797
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-10-06 16:48:59 +0200

    Fix local script

--- run_benchmarks_local.sh
+++ run_benchmarks_local.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 # This scripts runs the benchmarks on a remote cluster
 
-MANAGER_IP=172.16.2.53
+MANAGER_IP=172.16.2.98
 PEER_NUMBER=$1
+DELTA=$2
 
 
 if [ -z "$PEER_NUMBER" ]
   then
     echo "you have to indicate number of peers"
+    exit
+fi
+
+if [ -z "$DELTA" ]
+  then
+    echo "you have to indicate the EpTO delta"
     exit
 fi
 
@@ -22,9 +29,11 @@ docker swarm init && \
 docker network create -d overlay --subnet=10.0.93.0/24 epto-network
 
 docker service create --name epto-tracker --network epto-network --replicas 1 --limit-memory 300m tracker
+sleep 10s
 docker service create --name epto-service --network epto-network --replicas ${PEER_NUMBER} \
---env "PEER_NUMBER=${PEER_NUMBER}" --limit-memory 200m --log-driver=journald \
---restart-condition=none --stop-grace-period=30s --mount type=bind,source=/home/jocelyn/tmp/data,target=/data epto
+--env "PEER_NUMBER=${PEER_NUMBER}" --env "DELTA=$DELTA" \
+--limit-memory 250m --log-driver=journald --restart-condition=none \
+--mount type=bind,source=/home/jocelyn/tmp/data,target=/data epto
 
 echo "Running EpTO tester..."
 while true
