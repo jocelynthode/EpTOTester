733db163b1ca7d650647ba6cd342808fe7b33eaf
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-10-06 16:50:46 +0200

    Enhance Event tests

--- src/test/kotlin/EventTest.kt
+++ src/test/kotlin/EventTest.kt
@@ -6,6 +6,7 @@ import org.junit.Test
 import java.io.ByteArrayOutputStream
 import java.io.IOException
 import java.util.*
+import java.util.zip.GZIPOutputStream
 
 /**
  * Class testing the events methods
@@ -28,11 +29,6 @@ class EventTest {
         event1 = Event(UUID(44444, 324645), 2, 5, UUID(4444, 2222))
         event2 = Event(UUID(847392, 848123), 1, 5, UUID(4444, 2222))
         event3 = Event(UUID(23333, 233123), 1, 5, UUID(1111, 2222))
-
-        event4 = Event(UUID(2951, 29942), 500, 87, UUID(67754, 252453))
-        event5 = Event(UUID(444344, 324645), 300, 300, UUID(3456745, 453568))
-        event6 = Event(UUID(847392, 848123), 130, 9, UUID(2662, 57245))
-        event7 = Event(UUID(255333, 233123), 30, 45, UUID(61856, 8454))
     }
 
     @Test
@@ -52,126 +48,56 @@ class EventTest {
 
     @Test
     fun testEventOverhead() {
-        var byteOut = ByteArrayOutputStream()
-        var out = Application.conf.getObjectOutput(byteOut)
+        val EVENTS = 20
+        val rand = Random()
+        val byteOut = ByteArrayOutputStream()
+        val gzipOut = GZIPOutputStream(byteOut)
+        val out = Application.conf.getObjectOutput(gzipOut)
 
-        out.writeObject(event4, Event::class.java)
+        for (i in 1..EVENTS) {
+            val event = Event(UUID.randomUUID(), rand.nextInt(10), rand.nextInt(30), UUID.randomUUID())
+            out.writeObject(event, Event::class.java)
+        }
         out.close()
-        println(byteOut.toByteArray().size)
-
-        byteOut = ByteArrayOutputStream()
-        out = Application.conf.getObjectOutput(byteOut)
-
-        out.writeObject(event5, Event::class.java)
-        out.close()
-        println(byteOut.toByteArray().size)
-
-        byteOut = ByteArrayOutputStream()
-        out = Application.conf.getObjectOutput(byteOut)
-
-        out.writeObject(event6, Event::class.java)
-        out.close()
-        println(byteOut.toByteArray().size)
-
-        byteOut = ByteArrayOutputStream()
-        out = Application.conf.getObjectOutput(byteOut)
-
-        out.writeObject(event7, Event::class.java)
-        out.close()
-        println(byteOut.toByteArray().size)
+        println("Average event size: ${byteOut.toByteArray().size.toDouble() / EVENTS}")
 
         println("---------------")
 
-        var byteOut1 = ByteArrayOutputStream()
-        var out1 = Application.conf.getObjectOutput(byteOut1)
-
-        out1.writeLong(event4.id.mostSignificantBits)
-        out1.writeLong(event4.id.leastSignificantBits)
-        out1.writeLong(event4.timestamp)
-        out1.writeInt(event4.ttl)
-        out1.writeLong(event4.sourceId!!.mostSignificantBits)
-        out1.writeLong(event4.sourceId!!.leastSignificantBits)
+        val byteOut1 = ByteArrayOutputStream()
+        val gzipOut1 = GZIPOutputStream(byteOut1)
+        val out1 = Application.conf.getObjectOutput(gzipOut1)
+        for (i in 1..EVENTS) {
+            val id = UUID.randomUUID()
+            val timestamp = rand.nextInt(10)
+            val ttl = rand.nextInt(30)
+            val sourceId = UUID.randomUUID()
+            out1.writeLong(id.mostSignificantBits)
+            out1.writeLong(id.leastSignificantBits)
+            out1.writeInt(timestamp)
+            out1.writeInt(ttl)
+            out1.writeLong(sourceId.mostSignificantBits)
+            out1.writeLong(sourceId.leastSignificantBits)
+        }
         out1.close()
-        println(byteOut1.toByteArray().size)
-
-        byteOut1 = ByteArrayOutputStream()
-        out1 = Application.conf.getObjectOutput(byteOut1)
-
-        out1.writeLong(event5.id.mostSignificantBits)
-        out1.writeLong(event5.id.leastSignificantBits)
-        out1.writeLong(event5.timestamp)
-        out1.writeInt(event5.ttl)
-        out1.writeLong(event5.sourceId!!.mostSignificantBits)
-        out1.writeLong(event5.sourceId!!.leastSignificantBits)
-        out1.close()
-        println(byteOut1.toByteArray().size)
-
-        byteOut1 = ByteArrayOutputStream()
-        out1 = Application.conf.getObjectOutput(byteOut1)
-
-        out1.writeLong(event6.id.mostSignificantBits)
-        out1.writeLong(event6.id.leastSignificantBits)
-        out1.writeLong(event6.timestamp)
-        out1.writeInt(event6.ttl)
-        out1.writeLong(event6.sourceId!!.mostSignificantBits)
-        out1.writeLong(event6.sourceId!!.leastSignificantBits)
-        out1.close()
-        println(byteOut1.toByteArray().size)
-
-        byteOut1 = ByteArrayOutputStream()
-        out1 = Application.conf.getObjectOutput(byteOut1)
-
-        out1.writeLong(event7.id.mostSignificantBits)
-        out1.writeLong(event7.id.leastSignificantBits)
-        out1.writeLong(event7.timestamp)
-        out1.writeInt(event7.ttl)
-        out1.writeLong(event7.sourceId!!.mostSignificantBits)
-        out1.writeLong(event7.sourceId!!.leastSignificantBits)
-        out1.close()
-        println(byteOut1.toByteArray().size)
+        println("Average event size: ${byteOut1.toByteArray().size.toDouble() / EVENTS}")
 
 
         println("---------------")
 
-        var byteOut2 = ByteArrayOutputStream()
-        var out2 = Application.conf.getObjectOutput(byteOut2)
-
-        out2.writeObject(event4.id, UUID::class.java)
-        out2.writeLong(event4.timestamp)
-        out2.writeInt(event4.ttl)
-        out2.writeObject(event4.sourceId, UUID::class.java)
+        val byteOut2 = ByteArrayOutputStream()
+        val gzipOut2 = GZIPOutputStream(byteOut2)
+        val out2 = Application.conf.getObjectOutput(gzipOut2)
+        for (i in 1..EVENTS) {
+            val id = UUID.randomUUID()
+            val timestamp = rand.nextInt(10)
+            val ttl = rand.nextInt(30)
+            val sourceId = UUID.randomUUID()
+            out2.writeObject(id, UUID::class.java)
+            out2.writeInt(timestamp)
+            out2.writeInt(ttl)
+            out2.writeObject(sourceId, UUID::class.java)
+        }
         out2.close()
-        println(byteOut2.toByteArray().size)
-
-        byteOut2 = ByteArrayOutputStream()
-        out2 = Application.conf.getObjectOutput(byteOut2)
-
-        out2.writeObject(event5.id, UUID::class.java)
-        out2.writeLong(event5.timestamp)
-        out2.writeInt(event5.ttl)
-        out2.writeObject(event5.sourceId, UUID::class.java)
-        out2.close()
-        println(byteOut2.toByteArray().size)
-
-        byteOut2 = ByteArrayOutputStream()
-        out2 = Application.conf.getObjectOutput(byteOut2)
-
-        out2.writeObject(event6.id, UUID::class.java)
-        out2.writeLong(event6.timestamp)
-        out2.writeInt(event6.ttl)
-        out2.writeObject(event6.sourceId, UUID::class.java)
-        out2.close()
-        println(byteOut2.toByteArray().size)
-
-        byteOut2 = ByteArrayOutputStream()
-        out2 = Application.conf.getObjectOutput(byteOut2)
-
-        out2.writeObject(event7.id, UUID::class.java)
-        out2.writeLong(event7.timestamp)
-        out2.writeInt(event7.ttl)
-        out2.writeObject(event7.sourceId, UUID::class.java)
-        out2.close()
-        println(byteOut2.toByteArray().size)
-
+        println("Average event size: ${byteOut2.toByteArray().size.toDouble() / EVENTS}")
     }
 }
