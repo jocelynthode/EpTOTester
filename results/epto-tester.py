c887981b355502dc4536b44371744531a2be908f
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-10-12 10:42:07 +0200

    Add ratio received/sent balls

--- results/epto-tester.py
+++ results/epto-tester.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3.5
 import re
+import sys
 from pathlib import Path
-
-length = 0
 
 
 def extract_duration(lines):
@@ -10,7 +9,7 @@ def extract_duration(lines):
     end_at = 0
     start_at = 0
     for line in it:
-        match = re.match(r'(\d+) - sending:', line)
+        match = re.match(r'(\d+) - Sending:', line)
         if match:
             start_at = int(match.group(1))
             break
@@ -23,12 +22,32 @@ def extract_duration(lines):
 
 
 def all_durations():
-    for fpath in Path().glob('172.*.txt'):
-        global length
-        length += 1
+    for fpath in Path().glob(sys.argv[1]+'/**/172.*.txt'):
         with fpath.open() as f:
             yield extract_duration(f)
 
+
+def extract_balls(lines):
+    messages_sent = 0
+    messages_received = 0
+    it = iter(lines)
+    for line in it:
+        match = re.match(r'\d+ - EpTO messages sent: (\d+)',line)
+        if match:
+            messages_sent += int(match.group(1))
+            break
+    for line in it:
+        match = re.match(r'\d+ - EpTO messages received: (\d+)',line)
+        if match:
+            messages_received += int(match.group(1))
+            break
+    return messages_sent, messages_received
+
+
+def count_balls():
+    for fpath in Path().glob(sys.argv[1]+'/**/172.*.txt'):
+        with fpath.open() as f:
+            yield extract_balls(f)
 
 durations = list(all_durations())
 mininum = min(durations)
@@ -36,8 +55,20 @@ maximum = max(durations)
 average = sum(durations) / len(durations)
 
 # print(durations)
-print("EpTO run with %d peers" % length)
+peer_number = int(sys.argv[2])
+print("EpTO run with %d peers" % peer_number)
 print("------------------------")
 print("Least time to deliver: %d ms" % mininum)
 print("Most time to deliver: %d ms" % maximum)
 print("Average time to deliver: %d ms" % average)
+
+balls = list(count_balls())
+balls_sent, balls_received = zip(*balls)
+balls_received = list(balls_received)
+balls_sent = list(balls_sent)
+sent_sum= sum(balls_sent)
+received_sum = sum(balls_received)
+
+print("Total balls sent: %d" % sent_sum)
+print("Total balls received: %d" % received_sum)
+print("Total ratio received/sent: %f" % (received_sum / sent_sum))
