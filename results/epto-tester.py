dd7f0a78908d03428159f9ac28fdb356503e5186
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-10-17 16:53:39 +0200

    Display the Average global time

--- results/epto-tester.py
+++ results/epto-tester.py
@@ -4,8 +4,8 @@ import sys
 from pathlib import Path
 from collections import namedtuple
 
-Stats = namedtuple('Stats', ['start_at', 'end_at', 'duration', 'epto_sent', 'epto_received',
-                             'epto_sent_bytes', 'epto_received_bytes'])
+Stats = namedtuple('Stats', ['start_at', 'end_at', 'duration', 'epto_sent', 'epto_received'])
+PEER_NUMBER = int(sys.argv[2])
 
 
 def extract_stats(lines):
@@ -24,42 +24,49 @@ def extract_stats(lines):
     end_at = match_line(r'(\d+) - All events delivered !')
     messages_sent = match_line(r'\d+ - EpTO messages sent: (\d+)')
     messages_received = match_line(r'\d+ - EpTO messages received: (\d+)')
-    bytes_sent = match_line(r'\d+ - EpTO bytes sent: (\d+)')
-    bytes_received = match_line(r'\d+ - EpTO bytes received: (\d+)')
 
-    return Stats(start_at, end_at, end_at - start_at, messages_sent, messages_received, bytes_sent, bytes_received)
+    return Stats(start_at, end_at, end_at - start_at, messages_sent, messages_received)
 
 
 def all_stats():
     for fpath in Path().glob(sys.argv[1]):
         with fpath.open() as f:
             yield extract_stats(f)
+qg
 
-# TODO add global time between first send and last reception use start_at and end_at and fix problem when using more
-# TODO than 1 experiment
+def global_time(experiment_nb, stats):
+    for i in range(experiment_nb):
+        start_index = i * PEER_NUMBER
+        end_index = start_index + PEER_NUMBER
+        tmp = stats[start_index:end_index]
+        mininum_start = min([stat.start_at for stat in tmp])
+        maximum_end = max([stat.end_at for stat in tmp])
+        yield(maximum_end - mininum_start)
+
+
 stats = list(all_stats())
+experiments_nb = len(stats) // PEER_NUMBER
+global_times = list(global_time(experiments_nb, stats))
 durations = [stat.duration for stat in stats]
 mininum = min(durations)
 maximum = max(durations)
 average = sum(durations) / len(durations)
+global_average = sum(global_times) / len(global_times)
 
-
-peer_number = int(sys.argv[2])
-print("EpTO run with %d peers" % peer_number)
+print("EpTO run with %d peers across %d experiments" % (PEER_NUMBER, experiments_nb))
 print("------------------------")
-print("Least time to deliver: %d ms" % mininum)
-print("Most time to deliver: %d ms" % maximum)
-print("Average time to deliver: %d ms" % average)
+print("Least time to deliver in total : %d ms" % mininum)
+print("Most time to deliver in total : %d ms" % maximum)
+print("Average time to deliver per peer in total: %d ms" % average)
 
 messages_sent = [stat.epto_sent for stat in stats]
 messages_received = [stat.epto_received for stat in stats]
-bytes_sent = [stat.epto_sent_bytes for stat in stats]
-bytes_received = [stat.epto_received_bytes for stat in stats]
 
 sent_sum = sum(messages_sent)
 received_sum = sum(messages_received)
 print("Total balls sent: %d" % sent_sum)
-print("Total bytes sent: %d" % sum(bytes_sent))
 print("Total balls received: %d" % received_sum)
-print("Total bytes received: %d" % sum(bytes_received))
 print("Total ratio received/sent: %f" % (received_sum / sent_sum))
+
+print("------------")
+print("Average global time to deliver per experiment: %d ms" % global_average)
