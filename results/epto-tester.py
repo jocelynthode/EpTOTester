139f55433c5f4b5905e29cbd05e8f7f39d67c5e1
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-10-18 11:41:39 +0200

    Improve arg parser

--- results/epto-tester.py
+++ results/epto-tester.py
@@ -3,10 +3,17 @@ import re
 import sys
 from pathlib import Path
 from collections import namedtuple
+import argparse
 
-Stats = namedtuple('Stats', ['start_at', 'end_at', 'duration', 'epto_sent', 'epto_received'])
-PEER_NUMBER = int(sys.argv[2])
+Stats = namedtuple('Stats', ['start_at', 'end_at', 'duration', 'epto_sent', 'epto_received', 'bytes_tx', 'bytes_rx'])
 
+parser = argparse.ArgumentParser(description='Process EpTO logs')
+parser.add_argument('peer_number', metavar='PEER_NUMBER', type=int,
+                    help='the number of peer for an experiment')
+parser.add_argument('-g', '--glob-string', type=str, default='**/*.txt',
+                    help='The glob string to search for log files')
+args = parser.parse_args()
+PEER_NUMBER = args.peer_number
 
 def extract_stats(lines):
     it = iter(lines)  # Force re-use of same iterator
@@ -24,15 +31,17 @@ def extract_stats(lines):
     end_at = match_line(r'(\d+) - All events delivered !')
     messages_sent = match_line(r'\d+ - EpTO messages sent: (\d+)')
     messages_received = match_line(r'\d+ - EpTO messages received: (\d+)')
+    bytes_sent = match_line(r'(\d+) - Sent Bytes: (\d+)')
+    bytes_received = match_line(r'(\d+) - Received Bytes: (\d+)')
 
-    return Stats(start_at, end_at, end_at - start_at, messages_sent, messages_received)
+    return Stats(start_at, end_at, end_at - start_at, messages_sent, messages_received, bytes_sent, bytes_received)
 
 
 def all_stats():
-    for fpath in Path().glob(sys.argv[1]):
+    for fpath in Path().glob(args.glob_string):
         with fpath.open() as f:
             yield extract_stats(f)
-qg
+
 
 def global_time(experiment_nb, stats):
     for i in range(experiment_nb):
@@ -61,12 +70,21 @@ print("Average time to deliver per peer in total: %d ms" % average)
 
 messages_sent = [stat.epto_sent for stat in stats]
 messages_received = [stat.epto_received for stat in stats]
+bytes_sent = [stat.bytes_tx for stat in stats]
+bytes_received = [stat.bytes_rx for stat in stats]
 
 sent_sum = sum(messages_sent)
 received_sum = sum(messages_received)
+bytes_sent_sum = sum(bytes_sent)
+bytes_received_sum = sum(bytes_received)
 print("Total balls sent: %d" % sent_sum)
 print("Total balls received: %d" % received_sum)
-print("Total ratio received/sent: %f" % (received_sum / sent_sum))
-
+print("Total ratio balls received/sent: %f" % (received_sum / sent_sum))
+print("Total bytes sent: %d" % bytes_sent_sum)
+print("Total bytes received: %d" % bytes_received_sum)
+try:
+    print("Total ratio bytes received/sent %f" % (bytes_received_sum / bytes_sent_sum))
+except ZeroDivisionError:
+    pass
 print("------------")
-print("Average global time to deliver per experiment: %d ms" % global_average)
+print("Average global time to deliver on all peers per experiment: %d ms" % global_average)
