28191ef8f3837a804a14651d5710ec234e62ecf9
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-11-07 20:41:20 +0100

    Improve pss-tester

--- results/pss/pss-tester.py
+++ results/pss/pss-tester.py
@@ -3,7 +3,7 @@ import re
 import sys
 from pathlib import Path
 
-import matplotlib.pyplot as plt
+import argparse
 import networkx as nx
 import statistics as stats
 
@@ -12,20 +12,19 @@ graph = []
 
 def extract_nodes(from_seconds=0):
     def concat_lines():
-        for fpath in Path().glob('*.txt'):
-            with fpath.open() as f:
+        for fname in args.files:
+            with open(fname, 'r') as f:
                 yield from f
 
     lines = list(concat_lines())
 
     def extract_starts(lines):
         for line in lines:
-            match = re.match(r'(\d+) - Started:.*', line)
+            match = re.match(r'(\d+) - Running init PSS-1', line)
             if match:
                 yield int(match.group(1))
 
     start_at = min(extract_starts(lines))
-    print(start_at)
 
     min_time = start_at + from_seconds * 1000  # Timestamp are in milliseconds
     views = {}
@@ -37,7 +36,7 @@ def extract_nodes(from_seconds=0):
         timestamp = int(timestamp)
         source, *nodes = filter(None, nodes_str.split(' '))
 
-        if timestamp > min_time and source not in views:
+        if timestamp >= min_time and source not in views:
             views[source] = nodes
 
     for source, nodes in views.items():
@@ -49,11 +48,19 @@ def create_graph(from_seconds=30):
     return nx.parse_adjlist(extract_nodes(from_seconds), create_using=D)
 
 
-G = create_graph(int(sys.argv[1]))
+parser = argparse.ArgumentParser(description='Process PSS logs')
+parser.add_argument('from_seconds', metavar='FROM_SECONDS', type=int,
+                    help='from which point must the snapshot be taken')
+parser.add_argument('files', metavar='FILE', nargs='+', type=str,
+                    help='the files to parse')
+args = parser.parse_args()
+
+G = create_graph(args.from_seconds)
 U = G.to_undirected()
 
 print("Average shortest path: %f" % nx.average_shortest_path_length(G))
 print("Average clustering: %f" % nx.average_clustering(U))
+print("Is graph strongly connected: %s" % nx.is_strongly_connected(G))
 indeg = G.in_degree()
 print(indeg)
 print("Node nb: %d" % G.number_of_nodes())
