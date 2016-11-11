bd4462c44158ef9b232666a3851943157c5506c7
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-11-11 19:23:08 +0100

    Start working on Unified script for EpTO

--- cluster/churn.py
+++ cluster/churn.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3
-import argparse
+import glob
 import logging
 import random
+import re
 import subprocess
-import time
-
-from datetime import datetime
-from nodes_trace import NodesTrace
-
-
-LOCAL_DATA_FILES = '/home/jocelyn/tmp/data/*.txt'
 
 
 def churn_tuple(s):
@@ -24,12 +18,17 @@ class Churn:
     """
     Author: Jocelyn Thode
 
-    A class in charge of adding/suspending nodes to create churn in an EpTO cluster
+    A class in charge of adding/suspending nodes to create churn in a EpTO cluster
     """
     containers = {}
+    coordinator = None
+    peer_list = []
     periods = 0
+    logger = logging.getLogger('churn')
 
-    def __init__(self, hosts_filename=None):
+    def __init__(self, hosts_filename=None, service_name='epto'):
+
+        self.service_name = service_name
         self.hosts = ['localhost']
         if hosts_filename is not None:
             with open(hosts_filename, 'r') as file:
@@ -46,7 +45,7 @@ class Churn:
 
             choice = random.choice(self.hosts)
             if choice not in self.containers:
-                command_ps = ["docker", "ps", "-aqf", "name=epto-service", "-f", "status=running"]
+                command_ps = ["docker", "ps", "-aqf", "name={:s}".format(self.service_name), "-f", "status=running"]
                 if choice != 'localhost':
                     command_ps = ["ssh", choice] + command_ps
 
@@ -60,13 +59,13 @@ class Churn:
                 container = random.choice(self.containers[choice])
                 self.containers[choice].remove(container)
             except ValueError or IndexError:
-                logging.error("No container available")
+                self.logger.error('No container available')
                 return
 
             command_suspend += [container]
-            subprocess.call(command_suspend)
-            logging.info("Container {} on host {} was suspended"
-                         .format(container, choice))
+            subprocess.call(command_suspend, stdout=subprocess.DEVNULL)
+            self.logger.info('Container {} on host {} was suspended'
+                             .format(container, choice))
 
     def add_processes(self, to_create_nb):
         if to_create_nb < 0:
@@ -75,70 +74,14 @@ class Churn:
             return
         self.cluster_size += to_create_nb
         subprocess.call(["docker", "service", "scale",
-                         "epto-service={:d}".format(self.cluster_size)])
+                         "{:s}={:d}".format(self.service_name, self.cluster_size)],
+                        stdout=subprocess.DEVNULL)
+        self.logger.info('Service scaled up to {:d}'.format(self.cluster_size))
 
     def add_suspend_processes(self, to_suspend_nb, to_create_nb):
         self.suspend_processes(to_suspend_nb)
         self.add_processes(to_create_nb)
         self.periods += 1
 
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Create a churn',
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('delta', type=int, default=60,
-                        help='The interval between killing/adding new containers in s')
-    parser.add_argument('--local', '-l', action='store_true',
-                        help='Run the synthetic churn only on local node')
-    parser.add_argument('--synthetic', '-s', metavar='N', type=churn_tuple, nargs='+',
-                        help='Pass the synthetic list (to_kill,to_create)(example: 0,100 0,1 1,0)')
-    parser.add_argument('--delay', '-d', type=int,
-                        help='At which time should the churn start (UTC)')
-    parser.add_argument('--verbose', '-v', action='store_true',
-                        help='Switch DEBUG logging on')
-    args = parser.parse_args()
-
-    if args.verbose:
-        log_level = logging.DEBUG
-    else:
-        log_level = logging.INFO
-
-    logging.basicConfig(format='%(levelname)s: %(asctime)s - %(message)s', level=log_level)
-
-    if args.synthetic:
-        logging.info(args.synthetic)
-        nodes_trace = NodesTrace(synthetic=args.synthetic)
-    else:
-        nodes_trace = NodesTrace(database='database.db')
-
-    if args.local:
-        hosts_fname = None
-    else:
-        hosts_fname = 'hosts'
-
-    delta = args.delta
-    churn = Churn(hosts_filename=hosts_fname)
-
-    # Add initial cluster
-    churn.add_processes(nodes_trace.initial_size())
-    nodes_trace.next()
-
-    if args.delay:
-        delay = (datetime.utcfromtimestamp(args.delay // 1000) - datetime.utcnow()).seconds
-        if delay < 0:
-            delay = 0
-    else:
-        delay = 0
-
-    logging.info("Starting churn at {:s} UTC"
-                 .format(datetime.utcfromtimestamp(args.delay // 1000).isoformat()))
-    time.sleep(delay)
-    logging.info("Starting churn")
-
-    for _, to_kill, to_create in nodes_trace:
-        logging.debug("curr_size: {:d}, to_kill: {:d}, to_create {:d}"
-                      .format(_, len(to_kill), len(to_create)))
-        churn.add_suspend_processes(len(to_kill), len(to_create))
-        time.sleep(delta)
-
-    logging.info("Churn finished")
+    def set_logger_level(self, log_level):
+        self.logger.setLevel(log_level)
---
+++ cluster/run_benchmarks.py
@@ -0,0 +1,244 @@
+#!/usr/bin/env python3
+import argparse
+import docker
+import logging
+import re
+import signal
+import subprocess
+import threading
+import time
+import yaml
+
+from churn import Churn
+from churn import churn_tuple
+from datetime import datetime
+from docker import errors
+from docker import types
+from docker import utils
+from logging import config
+from nodes_trace import NodesTrace
+
+
+cli = docker.Client(base_url='unix://var/run/docker.sock')
+MANAGER_IP = '172.16.2.119'
+LOG_STORAGE = '/home/debian/data'
+REPOSITORY = 'swarm-m:5000/'
+SERVICE_NAME = 'epto'
+TRACKER_NAME = 'epto-tracker'
+NETWORK_NAME = 'epto_network'
+
+
+def create_service(service_name, image, env=None, mounts=None, placement=None, replicas=1):
+    container_spec = types.ContainerSpec(image=image, env=env, mounts=mounts)
+    logger.debug(container_spec)
+    task_tmpl = types.TaskTemplate(container_spec,
+                                   resources=types.Resources(mem_limit=314572800),
+                                   restart_policy=types.RestartPolicy(),
+                                   placement=placement)
+    logger.debug(task_tmpl)
+    cli.create_service(task_tmpl, name=service_name, mode={'Replicated': {'Replicas': replicas}},
+                       networks=[{'Target': NETWORK_NAME}])
+
+
+def run_churn(time_to_start):
+    logger.debug('Time to start churn: {:d}'.format(time_to_start))
+    if args.synthetic:
+        logger.info(args.synthetic)
+        nodes_trace = NodesTrace(synthetic=args.synthetic)
+    else:
+        nodes_trace = NodesTrace(database='database.db')
+
+    if args.local:
+        hosts_fname = None
+    else:
+        hosts_fname = 'hosts'
+
+    delta = args.delta
+    churn = Churn(hosts_filename=hosts_fname, service_name=SERVICE_NAME)
+    churn.set_logger_level(log_level)
+
+    # Add initial cluster
+    logger.debug('Initial size: {}'.format(nodes_trace.initial_size()))
+    churn.add_processes(nodes_trace.initial_size())
+    nodes_trace.next()
+    delay = (time_to_start - (time.time() * 1000)) // 1000
+    logger.debug('Delay: {:d}'.format(delay))
+    logger.info('Starting churn at {:s} UTC'
+                .format(datetime.utcfromtimestamp(time_to_start // 1000).isoformat()))
+    time.sleep(delay)
+    logger.info('Starting churn')
+
+    logger.debug(churn.peer_list)
+    churn.coordinator = churn.peer_list.pop(0)
+
+    for _, to_kill, to_create in nodes_trace:
+        logger.debug('curr_size: {:d}, to_kill: {:d}, to_create {:d}'
+                     .format(_, len(to_kill), len(to_create)))
+        churn.add_suspend_processes(len(to_kill), len(to_create))
+        time.sleep(delta)
+
+    logger.info('Churn finished')
+
+
+def wait_on_service(service_name, containers_nb, inverse=False):
+    def get_nb():
+        output = subprocess.check_output(['docker', 'service', 'ls', '-f', 'name={:s}'.format(service_name)],
+                                         universal_newlines=True).splitlines()[1]
+        return int(re.match(r'.+ (\d+)/\d+', output).group(1))
+
+    if inverse:  # Wait while current nb is equal to containers_nb
+        current_nb = containers_nb
+        while current_nb == containers_nb:
+            logger.debug('current_nb={:d}, containers_nb={:d}'.format(current_nb, containers_nb))
+            time.sleep(1)
+            current_nb = get_nb()
+    else:
+        current_nb = -1
+        while current_nb != containers_nb:
+            logger.debug('current_nb={:d}, containers_nb={:d}'.format(current_nb, containers_nb))
+            time.sleep(5)
+            current_nb = get_nb()
+
+
+def create_logger():
+    with open('logger.yaml') as f:
+        conf = yaml.load(f)
+        logging.config.dictConfig(conf)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run benchmarks',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('peer_number', type=int, help='With how many peer should it be ran')
+    parser.add_argument('time_add', type=int, help='Delay experiments start in ms')
+    parser.add_argument('events_to_send', type=int, help='How many events each peer should send')
+    parser.add_argument('rate', type=int, help='At which frequency should a peer send an event in ms')
+    parser.add_argument('-x', '--fixed-rate', type=int, default=None,
+                        help='Fix the rate at which a peer will send events')
+    parser.add_argument('-l', '--local', action='store_true',
+                        help='Run locally')
+    parser.add_argument('-n', '--runs', type=int, default=1, help='How many experiments should be ran')
+    parser.add_argument('--verbose', '-v', action='store_true', help='Switch DEBUG logging on')
+
+    subparsers = parser.add_subparsers(dest='churn', help='Specify churn and its arguments')
+
+    churn_parser = subparsers.add_parser('churn', help='Activate churn')
+    churn_parser.add_argument('delta', type=int, default=60,
+                              help='The interval between killing/adding new containers in s')
+    churn_parser.add_argument('--kill-coordinator', '-k', type=int, nargs='+',
+                              help='Kill the coordinator at the specified periods')
+    churn_parser.add_argument('--synthetic', '-s', metavar='N', type=churn_tuple, nargs='+',
+                              help='Pass the synthetic list (to_kill,to_create)(example: 0,100 0,1 1,0)')
+    churn_parser.add_argument('--delay', '-d', type=int, default=0,
+                              help='With how much delay compared to the tester should the tester start in ms')
+
+    args = parser.parse_args()
+    if not args.fixed_rate:
+        args.fixed_rate = args.peer_number
+    if args.verbose:
+        log_level = logging.DEBUG
+    else:
+        log_level = logging.INFO
+    create_logger()
+    logger = logging.getLogger('benchmarks')
+    logger.setLevel(log_level)
+
+    logger.info('START')
+
+    def signal_handler(signal, frame):
+        logger.info('Stopping Benchmarks')
+        try:
+            cli.remove_service(SERVICE_NAME)
+            cli.remove_service(TRACKER_NAME)
+            if not args.local:
+                time.sleep(15)
+                with open('hosts', 'r') as file:
+                    for host in file.read().splitlines():
+                        subprocess.call('rsync --remove-source-files '
+                                        '-av {:s}:{:s}/*.txt ../data'
+                                        .format(host, LOG_STORAGE), shell=True)
+                        subprocess.call('rsync --remove-source-files '
+                                        '-av {:s}:{:s}/capture/*.csv ../data/capture'
+                                        .format(host, LOG_STORAGE), shell=True)
+        except errors.NotFound:
+            pass
+        exit(0)
+    signal.signal(signal.SIGINT, signal_handler)
+
+    if args.local:
+        service_image = SERVICE_NAME
+        tracker_image = TRACKER_NAME
+        with subprocess.Popen(['../gradlew', '-p', '..', 'docker'],
+                              stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                              universal_newlines=True) as p:
+            for line in p.stdout:
+                print(line, end='')
+    else:
+        service_image = REPOSITORY + SERVICE_NAME
+        tracker_image = REPOSITORY + TRACKER_NAME
+        for line in cli.pull(service_image, stream=True, decode=True):
+            print(line)
+        for line in cli.pull(tracker_image, stream=True):
+            print(line)
+    try:
+        cli.init_swarm()
+        if not args.local:
+            logger.info('Joining Swarm on every hosts:')
+            token = cli.inspect_swarm()['JoinTokens']['Worker']
+            subprocess.call(['parallel-ssh', '-t', '0', '-h', 'hosts', 'docker', 'swarm',
+                             'join', '--token', token, '{:s}:2377'.format(MANAGER_IP)])
+        ipam_pool = utils.create_ipam_pool(subnet='172.111.0.0/16')
+        ipam_config = utils.create_ipam_config(pool_configs=[ipam_pool])
+        cli.create_network(NETWORK_NAME, 'overlay', ipam=ipam_config)
+    except errors.APIError:
+        logger.info('Host is already part of a swarm')
+        if not cli.networks(names=[NETWORK_NAME]):
+            logger.error('Network  doesn\'t exist!')
+            exit(1)
+
+    for run_nb, _ in enumerate(range(args.runs), 1):
+        create_service(TRACKER_NAME, tracker_image, placement={'Constraints': ['node.role == manager']})
+        wait_on_service(TRACKER_NAME, 1)
+        time_to_start = int((time.time() * 1000) + args.time_add)
+        logger.debug(datetime.utcfromtimestamp(time_to_start / 1000).isoformat())
+        environment_vars = {'PEER_NUMBER': args.peer_number, 'TIME': time_to_start,
+                            'EVENTS_TO_SEND': args.events_to_send, 'RATE': args.rate,
+                            'FIXED_RATE': args.fixed_rate}
+        environment_vars = ['{:s}={:d}'.format(k, v) for k, v in environment_vars.items()]
+        logger.debug(environment_vars)
+
+        service_replicas = 0 if args.churn else args.peer_number
+        create_service(SERVICE_NAME, service_image, env=environment_vars,
+                       mounts=[types.Mount(target='/data', source=LOG_STORAGE, type='bind')], replicas=service_replicas)
+
+        logger.info('Running EpTO tester -> Experiment: {:d}/{:d}'.format(run_nb, args.runs))
+        wait_on_service(SERVICE_NAME, 0, inverse=True)
+        if args.churn:
+            logger.info('Running with churn')
+            threading.Thread(target=run_churn, args=[time_to_start + args.delay], daemon=True).start()
+            # TODO find a way to stop at the right moment
+            wait_on_service(SERVICE_NAME, 10)
+        else:
+            logger.info('Running without churn')
+            wait_on_service(SERVICE_NAME, 0)
+
+        cli.remove_service(SERVICE_NAME)
+        cli.remove_service(TRACKER_NAME)
+
+        logger.info('Services removed')
+        time.sleep(30)
+
+        if not args.local:
+            subprocess.call('parallel-ssh -t 0 -h hosts "mkdir -p {path}/test-{nb}/capture &&'
+                            ' mv {path}/*.txt {path}/test-{nb}/ &&'
+                            ' mv {path}/capture/*.csv {path}/test-{nb}/capture/"'
+                            .format(path=LOG_STORAGE, nb=run_nb), shell=True)
+
+            subprocess.call('mkdir -p {path}/test-{nb}/capture'.format(path=LOG_STORAGE, nb=run_nb),
+                            shell=True)
+            subprocess.call('mv {path}/*.txt {path}/test-{nb}/'.format(path=LOG_STORAGE, nb=run_nb),
+                            shell=True)
+            subprocess.call('mv {path}/capture/*.csv {path}/test-{nb}/capture/'.format(path=LOG_STORAGE, nb=run_nb),
+                            shell=True)
+
+    logger.info('Benchmark done!')
+
